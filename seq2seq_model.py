8f7fadf9aa03b4867baaa6815ffcaf66fb15c3aa
吴万杰 <wanjie.wu@socialcredits.cn>
2018-08-31 16:57:21 +0800

    do more

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -4,48 +4,86 @@ import tensorflow as tf
 class RnnAttentionModel(object):
     def __init__(self, conf):
         self.config = conf
+        assert self.config.encoder_rnn_cell in ["GRU", "LSTM"]
+        assert self.config.attention_option in ["Luong", "Bahdanau"]
+        self._placeholder_layers()
+        self._embedding_layers()
+        self._encoder_layers()
+        self._create_attention_mechanism()
+        self._decoder_rnn_cell()
+        self.sess = tf.Session()
 
     def _placeholder_layers(self):
-        self.inputs = tf.placeholder(dtype=tf.int32, shape=[None, None], name="inputs")
-        self.targets = tf.placeholder(dtype=tf.int32, shape=[None], name="targets")
+        self.encoder_inputs = tf.placeholder(dtype=tf.int32, shape=[None, None], name="encoder_inputs")
+        self.decoder_target = tf.placeholder(dtype=tf.int32, shape=[None, None], name="decoder_target")
         self.keep_prob = tf.placeholder(dtype=tf.float32, shape=None, name="keep_prob")
 
-        self.seq_length = tf.reduce_sum(
-            tf.cast(tf.not_equal(tf.cast(0, self.inputs.dtype), self.inputs), tf.int32), axis=-1
+        self.encoder_seq_len = tf.reduce_sum(
+            tf.cast(tf.not_equal(tf.cast(0, self.encoder_inputs.dtype), self.encoder_inputs), tf.int32), axis=-1
+        )
+        self.decoder_seq_len = tf.reduce_sum(
+            tf.cast(tf.not_equal(tf.cast(0, self.decoder_target.dtype), self.decoder_target), tf.int32), axis=-1
         )
 
     def _embedding_layers(self):
         with tf.variable_scope(name_or_scope="embedding_layers"):
-            embedding_matrix = tf.get_variable(
-                name="embedding_matrix", shape=[self.config.vocab_size, self.config.embedding_size], dtype=tf.float32,
-                initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
+            encoder_emb_matrix = tf.get_variable(
+                name="encoder_emb_matrix", shape=[self.config.encoder_vocab_size, self.config.encoder_embedding_size],
+                dtype=tf.float32, initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
             )
-            self.embedded_inputs = tf.nn.embedding_lookup(params=embedding_matrix, ids=self.inputs)
-            # [B * T * D]
-            self.origin_shape = tf.shape(self.embedded_inputs)
+            self.encoder_emb_inp = tf.nn.embedding_lookup(params=encoder_emb_matrix, ids=self.encoder_inputs)
+            decoder_emb_matrix = tf.get_variable(
+                name="decoder_emb_matrix", shape=[self.config.decoder_vocab_size, self.config.decoder_embedding_size],
+                dtype=tf.float32, initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
+            )
+            self.decoder_emb_inp = tf.nn.embedding_lookup(params=decoder_emb_matrix, ids=self.decoder_target)
 
     def _encoder_layers(self):
-        with tf.variable_scope(name_or_scope="sentence_encoder_layers"):
-            cell_fw = tf.nn.rnn_cell.GRUCell(num_units=self.config.gru_num)
-            cell_bw = tf.nn.rnn_cell.GRUCell(num_units=self.config.gru_num)
-
-            (output_fw, output_bw), _ = tf.nn.bidirectional_dynamic_rnn(
-                cell_fw=cell_fw, cell_bw=cell_bw, inputs=self.embedded_inputs,
-                sequence_length=self.seq_length,
+        with tf.variable_scope(name_or_scope="encoder_layers"):
+            if self.config.encoder_rnn_cell == "GRU":
+                cell_fw = tf.nn.rnn_cell.GRUCell(num_units=self.config.encoder_rnn_num)
+                cell_bw = tf.nn.rnn_cell.GRUCell(num_units=self.config.encoder_rnn_num)
+            else:
+                cell_fw = tf.nn.rnn_cell.LSTMCell(num_units=self.config.encoder_rnn_num)
+                cell_bw = tf.nn.rnn_cell.LSTMCell(num_units=self.config.encoder_rnn_num)
+            (output_fw, output_bw), (state_fw, state_bw) = tf.nn.bidirectional_dynamic_rnn(
+                cell_fw=cell_fw, cell_bw=cell_bw, inputs=self.encoder_emb_inp,
+                sequence_length=self.encoder_seq_len,
                 dtype=tf.float32, time_major=False
             )
+            self.encoder_state = tf.concat([state_fw.h, state_bw.h], axis=1)
             self.encoder_outputs = tf.nn.dropout(tf.concat([output_fw, output_bw], axis=2), keep_prob=self.keep_prob)
 
-    def _attention_layers(self):
-        with tf.variable_scope("sentence_attention_layers"):
-            w_1 = tf.get_variable(
-                name="w_1", shape=[2 * self.config.gru_num, self.config.attention_size],
-                initializer=tf.truncated_normal_initializer(stddev=0.1)
-            )
-            b_1 = tf.get_variable(name="b_1", shape=[self.config.attention_size], initializer=tf.constant_initializer(0.))
-            u = tf.get_variable(
-                name="w_2", shape=[self.config.attention_size, 1], initializer=tf.truncated_normal_initializer(stddev=0.1))
-            v = tf.nn.xw_plus_b(tf.reshape(self.encoder_outputs, [-1, 2 * self.config.gru_num]), w_1, b_1)  # B*T*A
-            s = tf.matmul(v, u)
-            alphas = tf.nn.softmax(tf.reshape(s, [self.origin_shape[0], 1, self.origin_shape[1]]))
-            self.sentence_attention_output = tf.reduce_sum(tf.matmul(alphas, self.encoder_outputs), axis=1)
+    def _create_attention_mechanism(self):
+        if self.config.attention_option == "Luong":
+            self.attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+                num_units=self.config.attention_layer_size, memory=self.encoder_outputs,
+                memory_sequence_length=self.encoder_seq_len)
+        else:
+            self.attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+                num_units=self.config.attention_layer_size, memory=self.encoder_outputs,
+                memory_sequence_length=self.encoder_seq_len)
+
+    def _single_cell(self):
+        if self.config.cellType == "LSTM":
+            single_cell = tf.contrib.rnn.BasicLSTMCell(self.config.decoder_rnn_num)
+        else:
+            single_cell = tf.contrib.rnn.GRUCell(self.config.decoder_rnn_num)
+        self.single_cell = tf.contrib.rnn.DropoutWrapper(single_cell, self.config.keep_prob)
+
+    def _decoder_rnn_cell(self):
+        decoder_cell = tf.nn.rnn_cell.MultiRNNCell(self.single_cell)
+        self.decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
+            decoder_cell, self.attention_mechanism, self.config.attention_layer_size)
+        helper = tf.contrib.seq2seq.TrainingHelper(
+            inputs=self.decoder_emb_inp, sequence_length=self.decoder_seq_len, time_major=False)
+        decoder = tf.contrib.seq2seq.BasicDecoder(
+            cell=self.decoder_cell, helper=helper,
+            initial_state=self.decoder_cell.zero_state(self.config.batch_size, tf.float32).clone(self.encoder_state)
+        )
+        self.logits = tf.contrib.seq2seq.dynamic_decode(decoder=decoder, output_time_major=False)
+
+    def train(self, encoder_inputs, decoder_target):
+        self.sess.run(fetches=[], feed_dict={
+            self.encoder_inputs: encoder_inputs, self.decoder_target: decoder_target, self.keep_prob: 0.5
+        })
--- train_model.py
+++ train_model.py
@@ -1,8 +1,30 @@
 import tensorflow as tf
+from seq2seq_model import RnnAttentionModel
+
+tf.flags.DEFINE_string("attention_option", "Luong", "choose one type of attention Luong |Bahdanau")
+tf.flags.DEFINE_integer("attention_layer_size", 64, "attention size")
+
+tf.flags.DEFINE_string("encoder_rnn_cell", "LSTM", "LSTM/GRU")
+tf.flags.DEFINE_integer("encoder_rnn_num", 128, "encoder rnn hidden num")
+tf.flags.DEFINE_integer("encoder_vocab_size", 5000, "encoder vocab size")
+tf.flags.DEFINE_integer("encoder_embedding_size", 128, "dimension of vocab")
+
+tf.flags.DEFINE_integer("decoder_vocab_size", 5000, "decoder vocab size")
+tf.flags.DEFINE_integer("decoder_rnn_num", 128, "decoder vocab size")
+tf.flags.DEFINE_integer("decoder_embedding_size", 128, "decoder rnn hidden num")
+
+FLAG = tf.flags.FLAGS
 
 
 def main(_):
-    pass
+    input_x = [
+        [1, 2, 3], [2, 1, 4], [5, 6, 2], [6, 9, 8], [11, 3, 2]
+    ]
+    input_y = [
+        [1, 2], [5, 6], [6, 8], [11, 3]
+    ]
+    ram = RnnAttentionModel(FLAG)
+    ram.train(input_x, input_y)
 
 
 if __name__ == "__main__":
