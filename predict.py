691b1b85726d1e00b08b2ff9fababfd28814f167
吴万杰 <wanjie.wu@socialcredits.cn>
2018-09-07 17:52:33 +0800

    first version

---
+++ predict.py
@@ -0,0 +1,47 @@
+import os
+import codecs
+import tensorflow as tf
+
+
+class PredictModel(object):
+    def __init__(self):
+        self.model_dir = "model/"
+        self._currPath = os.path.dirname(__file__)
+        self._vocabDict = self.__load_chinese_vocab()
+
+    def __cnn_by_meta_graph(self):
+        checkpoint_file = tf.train.latest_checkpoint(self.model_dir)
+        graph = tf.Graph()
+        with graph.as_default():
+            self.sess = tf.Session()
+            with self.sess.as_default():
+                saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+                saver.restore(self.sess, checkpoint_file)
+                self.inputs = graph.get_operation_by_name("encoder_inputs").outputs[0]
+                self.keep_prob = graph.get_operation_by_name("keep_prob").outputs[0]
+                self.predictions = graph.get_operation_by_name("greedy_search/predictions").outputs[0]
+
+    def __load_chinese_vocab(self):
+        cv = dict()
+        with codecs.open(os.path.join(self._currPath, "data/chinese_vocab.txt"), "r", "utf8") as f:
+            for i, line in enumerate(f.readlines()):
+                cv[line.strip()] = i
+        return cv
+
+    def __convert_question(self, question):
+        _id_lst = []
+        for char in question:
+            _id = self._vocabDict.get(char, 3)  # index(<UNKNOWN>) == 3
+            _id_lst.append(_id)
+        return _id_lst
+
+    def predict_by_meta_graph(self, question):
+        question = self.__convert_question(question)
+        predictions = self.sess.run(self.predictions, {self.inputs: [question], self.keep_prob: 1.})
+        return predictions
+
+
+if __name__ == "__main__":
+    ques = "你好，我想问下，月经干净了，过个四五天又来了一点点，是什么问题"
+    pm = PredictModel()
+    pm.predict_by_meta_graph(ques)
--- seq2seq_model.py
+++ seq2seq_model.py
@@ -8,13 +8,15 @@ class RnnAttentionModel(object):
         assert self.config.unit_type in ["GRU", "LSTM"]
         assert self.config.encoder_type in ["Single", "Bi"]
         assert self.config.attention_option in ["Luong", "Bahdanau"]
+        self.checkpoint_path = "model/dialog"
         self.sess = tf.Session()
         self.SOS = 1
         self.EOS = 2
         self.PAD = 0
         self._placeholder_layers()
         self._embedding_layers()
-        self._inference()
+        self._train_inference()
+        self._prediction_decoder()
         self._build_train_op()
 
     def _placeholder_layers(self):
@@ -26,9 +28,6 @@ class RnnAttentionModel(object):
         self.keep_prob = tf.placeholder(dtype=tf.float32, shape=None, name="keep_prob")
         self.batch_size = tf.shape(self.encoder_inputs)[0]
         # <PAD> <SOS> <EOS> <UNKNOWN> 分别对应0, 1, 2, 3 //填充符-开始符-结束符-未登录符
-        self.start_token = tf.fill([self.batch_size, 1], self.SOS)
-        self.end_token = tf.fill([self.batch_size, 1], self.EOS)
-
         self.encoder_seq_len = tf.reduce_sum(
             tf.cast(tf.not_equal(tf.cast(self.PAD, self.encoder_inputs.dtype), self.encoder_inputs), tf.int32), axis=-1
         )
@@ -36,47 +35,43 @@ class RnnAttentionModel(object):
             tf.cast(tf.not_equal(tf.cast(self.PAD, self.decoder_train.dtype), self.decoder_train), tf.int32), -1
         )
 
-        self.decoder_train_inputs = tf.concat([self.start_token, self.decoder_train], axis=1)
-        self.decoder_train_targets = tf.concat([self.decoder_train, self.end_token], axis=1)
+        self.decoder_train_inputs = tf.concat([tf.fill([self.batch_size, 1], self.SOS), self.decoder_train], axis=1)
+        self.decoder_train_targets = tf.concat([self.decoder_train, tf.fill([self.batch_size, 1], self.EOS)], axis=1)
+        self.max_target_sequence_length = tf.reduce_max(self.decoder_seq_len)
         self.mask_seq_len = tf.sequence_mask(
             self.decoder_seq_len, tf.shape(self.decoder_train_targets)[1], dtype=tf.float32)
 
     def _embedding_layers(self):
         """转换字向量"""
         with tf.variable_scope(name_or_scope="embedding_layers"):
-            encoder_emb_matrix = tf.get_variable(
+            self.emb_matrix = tf.get_variable(
                 name="encoder_emb_matrix", shape=[self.config.vocab_size, self.config.embedding_size],
                 dtype=tf.float32, initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
             )
-            self.encoder_emb_inp = tf.nn.embedding_lookup(params=encoder_emb_matrix, ids=self.encoder_inputs)
+            self.encoder_emb_inp = tf.nn.embedding_lookup(params=self.emb_matrix, ids=self.encoder_inputs)
+            self.decoder_emb_inp = tf.nn.embedding_lookup(params=self.emb_matrix, ids=self.decoder_train_inputs)
 
-            decoder_emb_matrix = tf.get_variable(
-                name="decoder_emb_matrix", shape=[self.config.vocab_size, self.config.embedding_size],
-                dtype=tf.float32, initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1)
-            )
-            self.decoder_emb_inp = tf.nn.embedding_lookup(params=decoder_emb_matrix, ids=self.decoder_train_inputs)
-
-    def _inference(self):
-        """
-        推理
-        1. encoder
-        2. decoder
-        """
-        with tf.variable_scope("encoder_attention"):
+    def _train_infer(self):
+        with tf.variable_scope("train_encoder_attention"):
             self._build_encoder_layers()  # encoder layers
             attention_mechanism = self._create_attention_mechanism()  # attention mechanism
-
-        with tf.variable_scope("prepare_decoder"):
-            decoder_cell = self._combine_single_cell()  # 构建decoder cell
+        with tf.variable_scope("train_prepare_decoder"):
+            decoder_cell = self._create_rnn_cells()  # 构建decoder cell
             self.decoder_cell = tf.contrib.seq2seq.AttentionWrapper(
                 decoder_cell, attention_mechanism, self.config.num_units)
-
             self.decoder_initial_state = self.decoder_cell.zero_state(
                 self.batch_size, tf.float32).clone(cell_state=self.encoder_state)
 
             self.output_layer = tf.layers.Dense(
                 self.config.vocab_size, kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
 
+    def _train_inference(self):
+        """
+        推理
+        1. encoder
+        2. decoder
+        """
+        self._train_infer()
         with tf.variable_scope("training_decoder"):
             training_helper = tf.contrib.seq2seq.TrainingHelper(
                 inputs=self.decoder_emb_inp, sequence_length=self.decoder_seq_len+1, time_major=False)
@@ -85,7 +80,9 @@ class RnnAttentionModel(object):
                 initial_state=self.decoder_initial_state, output_layer=self.output_layer
             )
             # 返回 final_outputs, final_state, final_sequence_lengths
-            final_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder=training_decoder, impute_finished=True)
+            final_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+                decoder=training_decoder
+            )
         self.logits = final_outputs[0]
 
     def _build_train_op(self):
@@ -103,13 +100,13 @@ class RnnAttentionModel(object):
         with tf.variable_scope(name_or_scope="encoder_layers"):
             if self.config.encoder_type == "Single":
                 encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
-                    cell=self._combine_single_cell(), inputs=self.encoder_emb_inp,
+                    cell=self._create_rnn_cells(), inputs=self.encoder_emb_inp,
                     sequence_length=self.encoder_seq_len, dtype=tf.float32, time_major=False
                 )
             elif self.config.encoder_type == "Bi":
                 (output_fw, output_bw), encoder_state = tf.nn.bidirectional_dynamic_rnn(
-                    cell_fw=self._combine_single_cell()[0],
-                    cell_bw=self._combine_single_cell()[0],
+                    cell_fw=tf.contrib.rnn.LSTMCell(num_units=self.config.num_units),
+                    cell_bw=tf.contrib.rnn.LSTMCell(num_units=self.config.num_units),
                     inputs=self.encoder_emb_inp, sequence_length=self.encoder_seq_len,
                     dtype=tf.float32, time_major=False
                 )
@@ -117,29 +114,26 @@ class RnnAttentionModel(object):
             else:
                 raise ValueError("encoder type must be in [Single, Bi]")
 
-            if not self.config.beam_search:
-                self.encoder_state = encoder_state
-                self.encoder_outputs = encoder_outputs
-            else:
-                self.encoder_outputs = tf.contrib.seq2seq.tile_batch(encoder_outputs, multiplier=self.config.beam_width)
-                self.encoder_state = tf.contrib.seq2seq.tile_batch(encoder_state, multiplier=self.config.beam_width)
-                # from tensorflow.python.util import nest
-                # self.encoder_state = nest.map_structure(lambda s: tf.contrib.seq2seq.tile_batch(s, self.config.beam_width), encoder_state)
-                self.encoder_seq_len = tf.contrib.seq2seq.tile_batch(self.encoder_seq_len, multiplier=self.config.beam_width)
-                self.batch_size = self.batch_size * self.config.beam_width
+            self.encoder_state = encoder_state
+            self.encoder_outputs = encoder_outputs
 
-    def _combine_single_cell(self):
-        return tf.contrib.rnn.MultiRNNCell([self._single_cell() for _ in range(self.config.num_layers)])
+    def _saver(self):
+        saver = tf.train.Saver()
+        saver.save(sess=self.sess, save_path=self.checkpoint_path)
 
-    def _single_cell(self):
-        with tf.variable_scope(name_or_scope="single_cell"):
+    def _create_rnn_cells(self):
+        def single_rnn_cell():
             if self.config.unit_type == "GRU":
                 single_cell = tf.contrib.rnn.GRUCell(num_units=self.config.num_units)
             elif self.config.unit_type == "LSTM":
-                single_cell = tf.contrib.rnn.BasicLSTMCell(num_units=self.config.num_units)
+                single_cell = tf.contrib.rnn.LSTMCell(num_units=self.config.num_units)
             else:
                 raise ValueError("unit_type must in [GRU, LSTM]")
-            return tf.contrib.rnn.DropoutWrapper(single_cell, self.keep_prob)
+            single_cell = tf.contrib.rnn.DropoutWrapper(single_cell, self.keep_prob)
+            return single_cell
+
+        with tf.variable_scope(name_or_scope="create_rnn_cells"):
+            return tf.contrib.rnn.MultiRNNCell([single_rnn_cell() for _ in range(self.config.num_layers)])
 
     def _create_attention_mechanism(self):
         if self.config.attention_option == "Luong":
@@ -150,52 +144,46 @@ class RnnAttentionModel(object):
             num_units=self.config.num_units, memory=self.encoder_outputs,
             memory_sequence_length=self.encoder_seq_len)
 
-    def __search_decoder(self):
-        if self.config.beam_search:
-            inference_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
-                cell=self.decoder_cell, embedding=self.decoder_emb_inp, start_token=self.start_token,
-                end_token=self.EOS, initial_state=self.decoder_initial_state, beam_width=self.config.beam_width,
-                output_layer=self.output_layer
-            )
-        else:
+    def _prediction_decoder(self):
+        start_tokens = tf.fill([self.batch_size], self.SOS)
+        with tf.variable_scope("greedy_search"):
             decoding_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
-                embedding=self.decoder_emb_inp, start_tokens=self.start_token, end_token=self.EOS)
+                embedding=self.emb_matrix, start_tokens=start_tokens, end_token=self.EOS)
             inference_decoder = tf.contrib.seq2seq.BasicDecoder(
                 cell=self.decoder_cell, helper=decoding_helper, initial_state=self.decoder_initial_state,
                 output_layer=self.output_layer)
 
-        final_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
-            decoder=inference_decoder)
+            final_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder=inference_decoder)
+            self.greedy_predict = tf.expand_dims(final_outputs.sample_id, -1, name="predictions")
 
     def train(self, flag):
         self.sess.run(tf.global_variables_initializer())
         print("begin train ...")
         step = 0
         _iter = 0
+        summary_writer = tf.summary.FileWriter(flag.log_dir, graph=self.sess.graph)
         for i in range(flag.epoch):
             pqd = PrePareQaData(flag, "train")
             for encoder_input, decoder_target in pqd:
                 step += len(encoder_input)
                 _iter += 1
-                loss = self.sess.run(fetches=[self.logits], feed_dict={
+                _, summary, loss = self.sess.run(fetches=[self.train_op, self.summary_op, self.loss], feed_dict={
                     self.encoder_inputs: encoder_input, self.decoder_train: decoder_target, self.keep_prob: 0.5
                 })
-                import pdb
-                pdb.set_trace()
+                summary_writer.add_summary(summary, global_step=step)
                 print("<Train>\t Epoch:[%d] Iter[%d] Step:[%d] Loss[%.3f]" % (i+1, _iter, step, loss))
+            self._saver()
 
     def test(self, flag):
-        print("begin train ...")
+        print("begin test ...")
         step = 0
         _iter = 0
-        pqd = PrePareQaData(flag, "test")
+        self.sess.run(tf.global_variables_initializer())
+        pqd = PrePareQaData(flag, "train")
         for encoder_input, decoder_target in pqd:
             step += len(encoder_input)
             _iter += 1
-            loss = self.sess.run(fetches=[self.loss], feed_dict={
+            loss = self.sess.run(fetches=self.loss, feed_dict={
                 self.encoder_inputs: encoder_input, self.decoder_train: decoder_target, self.keep_prob: 1.
             })
-            print("<Train>\t Iter[%d] Step:[%d] Loss[%.3f]" % (_iter, step, loss))
-
-    def predict(self):
-        pass
+            print("<Test>\t Iter[%d] Step:[%d] Loss[%.3f]" % (_iter, step, loss))
